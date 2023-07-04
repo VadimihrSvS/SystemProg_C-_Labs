d352272a6bedc5c33ab4abbbbd8c1b421d393c3a
VadimihrSvS <95396632+VadimihrSvS@users.noreply.github.com>
2023-07-04 13:34:52 +0300

    Add files via upload

---
+++ Lab_2-2/Sokolovsky_thread_lb/Resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Sokolovsky_thread_lb.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_SOKOLOVSKYTHREADLB_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_SOKOLOVSKYTHREADLB			107
+#define IDI_SMALL				108
+#define IDC_SOKOLOVSKYTHREADLB			109
+#define IDC_MYICON				2
+#define IDM_THREAD_CREATE			10000
+#define IDM_THREAD_CREATE_WAITING  10001
+#define IDM_THREAD_PAUSE 10002
+#define IDM_THREAD_RESUME 10003
+#define IDM_THREAD_DESTROY 10004
+#define IDM_THREAD_UP_PRIORITY 10005
+#define IDM_THREAD_DOWN_PRIORITY 10006
+#define IDM_THREAD_INFO 10014
+#define IDM_THREAD1_INFO 10015
+#define IDM_THREAD2_INFO 10016
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Следующие стандартные значения для новых объектов
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		10017
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
